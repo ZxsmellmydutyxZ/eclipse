d2374afc8bb6e6e1e4cb3fcdc12d2a1328db1b67
Aaron Barnett <aaron@outerbody.com>
2014-03-13 15:27:31 -0400

    fdsafdsa

--- somecodes/src/somecodes/yup.java
+++ somecodes/src/somecodes/yup.java
@@ -3,9 +3,9 @@ package somecodes;
 public class yup {
 
 	public static void main(String[] args) {
-		for(int i = 0; i<30; i++)
-		System.out.println("yup");
-		
+		for(int i = 0; i<30; i++){
+			System.out.println("yup");
+		}
 	}
 
 }
