ed34b7d49ac75d100fc46918457856b0f44b2922
ebarne0781 <ebarne0781@AHHDB11C6BFB99.inst.hcpss.org>
2014-03-21 09:53:23 -0400

    gui master

---
+++ GUI/src/Name.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+public class Name {
+
+	/**
+	 * Elam Barnett
+	 */
+	public static class DisplayName {
+		JFrame frame;
+		JPanel contentPane;
+		JLabel label;
+	
+	public DisplayName(){
+		frame = new JFrame("My Name is...");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		contentPane = new JPanel();
+		label = new JLabel("Elam Barnett");
+		contentPane.add(label);
+		
+		frame.setContentPane(contentPane);
+		
+		frame.pack();
+		frame.setVisible(true);
+}		
+	
+	private static void runGUI(){
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		DisplayName name = new DisplayName();
+	}
+
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				runGUI();
+			}
+		});
+	}
+}
+}
+
+
