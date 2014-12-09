e0b0fd1710319e7e8290078bbd36e89b8e2bd662
DuTy <DuTy@carl.mshome.net>
2014-12-09 15:58:34 -0500

    asdf

--- nov/src/arraylist.java
+++ nov/src/arraylist.java
@@ -1,10 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class arraylist {
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 	
 
 	
@@ -18,8 +20,10 @@ public class arraylist {
 	String d;
 	String e;
 	
+	
+	
 	while(true){
-		System.out.println("What do you want to do? (1 = list 2 = add)");
+		System.out.println("What do you want to do? (1 = list 2 = add 3 = save)");
 		a = scan.nextInt();
 		b = scan.nextLine();
 		switch(a){
@@ -34,7 +38,17 @@ public class arraylist {
 			b = scan.nextLine();
 			ar.add(b);
 			break;
+			
+		case 3:
+			FileWriter outFile = new FileWriter("/users/DuTy/Data.txt");  
+		    BufferedWriter outStream = new BufferedWriter(outFile);  
+		    for (int k = 0; k < ar.size(); k++)  
+		        outStream.write(ar.get(k)+ "\r\n");     
+		    outStream.close();  
+		    System.out.println("Data saved.");  
 		}
+		
+		
 	}
 	
 	
